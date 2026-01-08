13feffef61b956b6d66d6459c725e2ceb39e7900
Niklas Unneland <niklasu@Niklas-sin-MacBook-Air.local>
2026-01-09 00:24:29 +0100

    added go-back button

--- app/tester/page.tsx
+++ app/tester/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+'use client';
+
+import { useRouter } from 'next/navigation';
+import {router} from "next/dist/client";
+
+
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <p>Dette er litt sånn tekst not gonna lie</p>
+      <button type="button" onClick={() => router.back()}>
+          Gå tilbake, ingenting her
+      </button>
     </div>
   );
 }
